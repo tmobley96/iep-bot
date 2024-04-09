4dae17514d8674714b410d0f8a13e325a6644932
tmobley96 <124856260+tmobley96@users.noreply.github.com>
2024-04-09 15:04:57 -0500

    Update 1_Home.py

---
+++ 1_Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Set page title and layout
+st.set_page_config(page_title="IEP_Bot Homepage", page_icon="🎓", layout="centered")
+
+# Page title
+st.title("IEP_BOT Homepage")
